0be70fd396b54a40074f54330da98dc4b421f0da
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-09 19:06:46 -0500

    Adding item object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
-import odinImage from "./odin.png";
 
-console.log(greeting);
-   
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
+class Item {
+    constructor(title, description, dueDate, priority, notes, completed) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+        this.notes = notes
+        this.completed = completed
+        this.toggleCompleteStatus = function() {
+            this.completed = this.completed === "Completed" ? "Not Completed" : "Completed"
+        }
+    }
+}
